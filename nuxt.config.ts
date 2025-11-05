5f30cb6280e1429d2f2fc733773b9f6c86969038
AMBRESH <ambreshtumukur@gmail.com>
2025-11-06 00:08:23 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,13 @@ export default defineNuxtConfig({
         { name: 'description', content: 'A simple Nuxt app deployed on AWS EC2' }
       ]
     }
-  }
+  },
+  nitro: {
+    publicAssets: [
+      {
+        dir: 'public',   // ✅ Tell Nuxt to serve everything under /public
+        maxAge: 3600,    // Cache for 1 hour (optional)
+      },
+    ],
+  },
 })
